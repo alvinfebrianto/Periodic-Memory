1dae5a202b53b1e7e2a5c0a706e20de932cb61df
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2023-10-13 10:26:51 +0700

    9

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-const moves = document.getElementById("moves-count");
-const timeValue = document.getElementById("time");
-const startButton = document.getElementById("start");
-const stopButton = document.getElementById("stop");
-const gameContainer = document.querySelector(".game-container");
-const result = document.getElementById("result");
-const controls = document.querySelector(".controls-container");
-let cards;
-let interval;
-let firstCard = false;
-let secondCard = false;
+const moves = document.getElementById("moves-count"),
+      timeValue = document.getElementById("time"),
+      startButton = document.getElementById("start"),
+      stopButton = document.getElementById("stop"),
+      gameContainer = document.querySelector(".game-container"),
+      result = document.getElementById("result"),
+      controls = document.querySelector(".controls-container");
 
-// tambah untuk pasangan kartu yang cocok 
-let matchedPairs = 0;
+let cards, interval, firstCard = false, secondCard = false, matchedPairs = 0;
 
 const items = [
   { name: "hydrogen", image: "assets/golongan_1/g1H.png" },
@@ -23,148 +18,117 @@ const items = [
   { name: "francium", image: "assets/golongan_1/g1Fr.png" },
 ];
 
-//Inisialisasi waktu awal
-let seconds = 0,
-    minutes = 0;
-//Inisialisasi jumlah gerakan
-let movesCount = 0,
-    winCount = 0;
+let seconds = 0, minutes = 0, movesCount = 0, winCount = 0;
 
-//Fungsi untuk mengupdate waktu
-const timeGenerator = () => {
-  if (matchedPairs < cardValues.length / 2) {
-    seconds += 1;
-    if (seconds >= 60) {
-      minutes += 1;
-      seconds = 0;
-    }
-    //Format waktu
-    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
-    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
-    timeValue.innerHTML = `<span>Time: </span>${minutesValue}:${secondsValue}`;
-  }
-};
-
-//Fungsi untuk menghitung gerakan
 const movesCounter = () => {
   movesCount += 1;
   moves.innerHTML = `<span>Moves: </span>${movesCount}`;
 };
 
-//Fungsi untuk menampilkan kartu pasangan yang cocok
-function showSuccessNotification() {
-  if ("Notification" in window) {
-    //Periksa apakah browser mendukung Notification API
-    if (Notification.permission === "granted") {
-      //Izin notifikasi sudah diberikan
-      new Notification("Congratulations", {
-        body: "You've matched all the cards!",
-      });
-    } else if (Notification.permission !== "denied") {
-      //Minta izin notifikasi jika belum diberikan atau ditolak
-      Notification.requestPermission().then(function (permission) {
-        if (permission === "granted") {
-          new Notification("Congratulations", {
-            body: "You've matched all the cards!",
-          });
-        }
-      });
-    }
+const timeGenerator = () => {
+  if (matchedPairs < cards.length / 2) {
+    (seconds += 1) >= 60 && (minutes += 1, seconds = 0);
+    let e = seconds < 10 ? `0${seconds}` : seconds,
+      t = minutes < 10 ? `0${minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time: </span>${t}:${e}`;
   } else {
-    // Browser tidak mendukung Notification API
-    alert("Congratulations, you've matched all the cards!");
+    stopGame();
   }
-}
-
-//Pilih objek acak dari item array
-const generateRandom = (size = 4) => {
-  let tempArray = [...items];
-  let cardValues = [];
-  size = (size * size) / 2;
-
-  for (let i = 0; i < size; i++) {
-    const randomIndex = Math.floor(Math.random() * tempArray.length);
-    cardValues.push(tempArray[randomIndex]);
-    tempArray.splice(randomIndex, 1);
-  }
-  return cardValues;
 };
 
-const matrixGenerator = (cardValues, size = 4) => {
+const stopGame = () => {
+  controls.classList.remove("hide");
+  stopButton.classList.add("hide");
+  startButton.classList.remove("hide");
+  clearInterval(interval);
+  result.innerHTML = `<h2>You Won</h2><h4>Moves: ${movesCount}</h4><br><br><br>`;
+  matchedPairs = 0;
   gameContainer.innerHTML = "";
-  cardValues = [...cardValues, ...cardValues];
-  cardValues.sort(() => Math.random() - 0.5);
+};
 
-  for (let i = 0; i < size * size; i++) {
-    if (cardValues[i]) {
+const generateRandom = (e = 7) => {
+  let t = [...items], s = [];
+  e = e * e / 2;
+  for (let a = 0; a < e; a++) {
+    let n = Math.floor(Math.random() * t.length);
+    s.push(t[n], t[n]);
+    t.splice(n, 1);
+  }
+  return s.sort(() => Math.random() - 0.5);
+};
+
+const matrixGenerator = (e, t = 4) => {
+  gameContainer.innerHTML = "";
+
+  const rowCount = [4, 4, 4, 2];
+  let cardIndex = 0;
+
+  for (let row = 0; row < rowCount.length; row++) {
+    for (let col = 0; col < rowCount[row]; col++) {
       gameContainer.innerHTML += `
-        <div class="card-container" data-card-value="${cardValues[i].name}">
+        <div class="card-container" data-card-value="${e[cardIndex].name}">
           <div class="card-before">?</div>
           <div class="card-after">
-            <img src="${cardValues[i].image}" class="image"/>
+            <img src="${e[cardIndex].image}" class="image"/>
           </div>
         </div>
       `;
+      cardIndex++;
     }
   }
-  //Grid
-  gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
 
-  //Cards
+  gameContainer.style.gridTemplateColumns = `repeat(${t}, auto)`;
   cards = document.querySelectorAll(".card-container");
-  cards.forEach((card) => {
+
+  cards.forEach(card => {
     card.addEventListener("click", () => {
-      if (!card.classList.contains("matched") && !firstCard) {
-        card.classList.add("flipped");
-        firstCard = card;
-      } else if (!card.classList.contains("matched") && !secondCard && card !== firstCard) {
-        card.classList.add("flipped");
-        secondCard = card;
-        const firstCardValue = firstCard.getAttribute("data-card-value");
-        const secondCardValue = secondCard.getAttribute("data-card-value");
+      if (card.classList.contains("matched") || firstCard) {
+        if (!card.classList.contains("matched") && !secondCard && card !== firstCard) {
+          card.classList.add("flipped");
+          secondCard = card;
+          let s = firstCard.getAttribute("data-card-value");
+          let a = secondCard.getAttribute("data-card-value");
 
-        if (firstCardValue === secondCardValue) {
-          firstCard.classList.add("matched");
-          secondCard.classList.add("matched");
-          firstCard = false;
-          secondCard = false;
-          matchedPairs++;
-
-          if (matchedPairs === cardValues.length / 2) {
-            showSuccessNotification();
-            clearInterval(interval); // Hentikan timer saat permainan selesai
-          }
-        } else {
-          setTimeout(() => {
-            firstCard.classList.remove("flipped");
-            secondCard.classList.remove("flipped");
+          if (s === a) {
+            firstCard.classList.add("matched");
+            secondCard.classList.add("matched");
             firstCard = false;
             secondCard = false;
-          }, 1000);
+            matchedPairs += 1;
+
+            if (matchedPairs === e.length / 2) {
+              clearInterval(interval);
+            }
+          } else {
+            setTimeout(() => {
+              firstCard.classList.remove("flipped");
+              secondCard.classList.remove("flipped");
+              firstCard = false;
+              secondCard = false;
+            }, 1000);
+          }
+          movesCounter();
         }
-        movesCounter();
+      } else {
+        card.classList.add("flipped");
+        firstCard = card;
       }
     });
   });
 };
 
-//Start game
 startButton.addEventListener("click", () => {
   movesCount = 0;
   seconds = 0;
   minutes = 0;
-  //Kontrol dan tombol
   controls.classList.add("hide");
   stopButton.classList.remove("hide");
   startButton.classList.add("hide");
-  //Mulai timer
   interval = setInterval(timeGenerator, 1000);
-  //Gerakan awal
   moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
   initializer();
 });
 
-//Stop game
 stopButton.addEventListener("click", () => {
   controls.classList.remove("hide");
   stopButton.classList.add("hide");
@@ -175,11 +139,10 @@ stopButton.addEventListener("click", () => {
   gameContainer.innerHTML = "";
 });
 
-//Inisialisasi values
 const initializer = () => {
   result.innerText = "";
   winCount = 0;
-  cardValues = generateRandom();
+  let cardValues = generateRandom();
   console.log(cardValues);
   matrixGenerator(cardValues);
 };
